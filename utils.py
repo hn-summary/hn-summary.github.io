b5cf5201899088fb4cedab91c6f7ffcae86948eb
Santiago Basulto <santiago.basulto@gmail.com>
2022-07-31 13:24:25 +0200

    Cleanup

--- utils.py
+++ utils.py
@@ -1,33 +1,6 @@
 import pandas as pd
 from datetime import date, timedelta
 
-ALL_YEARS = [
-    2006,
-    2007,
-    2008,
-    2009,
-    2010,
-    2011,
-    2012,
-    2013,
-    2014,
-    2015,
-    2016,
-    2017,
-    2018,
-    2019,
-    2020,
-    2021,
-    2022
-]
-
-_all_months = [date(2021, i, 1) for i in range(1, 13)]
-ALL_MONTHS = [{
-    'numeric': "{:02d}".format(month.month),
-    'name': month.strftime('%b')
-} for month in _all_months]
-del _all_months
-
 
 def _get_weeks_in_a_year(year):
     return (date(year, 12, 31) - date(year, 1, 1)).days // 7
@@ -112,56 +85,3 @@ def generate_top_charts(main_df, domain_groups, start, end):
         }
 
     return results
-
-def get_header_data(year):
-    return {
-        'ALL_YEARS': ALL_YEARS,
-        'ALL_MONTHS': ALL_MONTHS,
-        'ALL_WEEKS': list(range(1, _get_weeks_in_a_year(year)+1))
-    }
-
-def generate_page_context_year(main_df, domain_groups, year):
-    start, end = get_date_range_for_year(year)
-
-    top_charts = generate_top_charts(main_df, domain_groups, start, end)
-
-    context = {
-        "start": start,
-        "end": end,
-        "top_charts": top_charts,
-        "selected_year": year,
-        **get_header_data(year),
-    }
-    return context
-
-
-def generate_page_context_month(main_df, domain_groups, year, month):
-    start, end = get_date_range_for_month(year, month)
-
-    top_charts = generate_top_charts(main_df, domain_groups, start, end)
-
-    context = {
-        "start": start,
-        "end": end,
-        "top_charts": top_charts,
-        "selected_year": year,
-        "selected_month": "{:02d}".format(month),
-        **get_header_data(year),
-    }
-    return context
-
-
-def generate_page_context_week(main_df, domain_groups, year, week):
-    start, end = get_date_range_for_week(year, week)
-
-    top_charts = generate_top_charts(main_df, domain_groups, start, end)
-
-    context = {
-        "start": start,
-        "end": end,
-        "top_charts": top_charts,
-        "selected_year": year,
-        "selected_week": week,
-        **get_header_data(year),
-    }
-    return context
